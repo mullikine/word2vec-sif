54895e44c9b06453ee44b6b6d13d43ba62e2c2b0
Shane Mulligan <mullikine@gmail.com>
2020-09-06 23:43:41 +1200

    Sun 06 Sep 2020 23:43:41 NZST

--- semantic_search.py
+++ semantic_search.py
@@ -17,7 +17,7 @@ import multiprocessing as mp
 
 def clean_utterance(doc):
     """
-    This function takes a SpaCy document and cleans it by removing 
+    This function takes a SpaCy document and cleans it by removing
     stop words, non-alphnumerical values, and returns the tokens to their lemmas.
 
     :doc: Spacy document https://spacy.io/api/doc
@@ -28,7 +28,7 @@ def clean_utterance(doc):
 def pre_process_docs(docs):
     """
     Pre-processes the list of string input documents using SpaCy.
-    It runs in parallel if there are a lot of documents.  
+    It runs in parallel if there are a lot of documents.
 
     :docs: list of strings.
     :return: list of Spacy documents https://spacy.io/api/doc
@@ -42,9 +42,9 @@ def pre_process_docs(docs):
 def multi_process_func(func, values):
     """
     This function takes a function func and an iterable object.
-    This function will instantiate n-cores - 1 processes to 
-    run the function on the list of values and then join the result 
-    and return it. 
+    This function will instantiate n-cores - 1 processes to
+    run the function on the list of values and then join the result
+    and return it.
 
     :func: function you want to call on values.
     :values: list of values to pass to func.
@@ -64,32 +64,32 @@ def multi_process_func(func, values):
 def clean_data(docs):
     """
     Clean the list of string input documents using SpaCy.
-    It runs in parallel if there are a lot of documents.  
+    It runs in parallel if there are a lot of documents.
 
     :docs: list of strings.
     :return: list of lists of string tokens that have been cleaned.
     """
     nlp_docs = pre_process_docs(docs)
-    if len(docs) > 1000: 
+    if len(docs) > 1000:
         return multi_process_func(clean_utterance, nlp_docs)
     else:
         return [clean_utterance(doc) for doc in nlp_docs]
 
 def create_w2v_model(tokenised_docs, size = 300):
     """
-    Trains a word2vec model using Gensim's implementation. 
+    Trains a word2vec model using Gensim's implementation.
 
     :tokenised_docs: list of lists of string tokens.
     :size: vector size to represent the words.
     :return: gensim word2vec model
     """
-    # Train word vectors 
+    # Train word vectors
     model = Word2Vec(tokenised_docs, size = size, min_count=1, workers= mp.cpu_count() - 1)
     return model
 
 def create_sif_model(model, tokenised_docs):
     """
-    Takes a Gensim word2vec model and creates a sif model. 
+    Takes a Gensim word2vec model and creates a sif model.
     implementation can be found here:
 
     :model: Gensim word2vec model
@@ -117,19 +117,19 @@ def query_documents(query_string, model, indexed_list, n_results = 10):
     :model: word2vec-sif model
     :indexed_list: a list representation of the documents.
     :n_results: number of results you want to return
-    :return: list of n best matching documents. 
+    :return: list of n best matching documents.
     """
     return model.sv.similar_by_sentence(query_string.split(), \
         model=model, indexable=indexed_list.items, topn=n_results)
 
 def get_sif_model(docs):
     """
-    Takes a list of documents and produces a word2vec-sif model with 
-    the document embeddings, and the indexed list representation 
+    Takes a list of documents and produces a word2vec-sif model with
+    the document embeddings, and the indexed list representation
     of the documents.
 
     :docs: list of strings.
-    :return: word2vec-sif model with 
+    :return: word2vec-sif model with
     the document embeddings, and the indexed list representation
     """
     myembed(globals(), locals())
